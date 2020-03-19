a59274b0c9cd08855a7b588855f2bcd6d92d0d01
jody <jody@mail>
2020-03-19 15:49:13 -0400

    Completed Module 3

---
+++ OdeToFood.Core/Restaurant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OdeToFood.Core
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Location { get; set; }
+        public CuisineType Cuisine { get; set; }
+    }
+}
---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,42 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(String name);
+        Restaurant GetById(int id);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name =  "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian },
+                new Restaurant { Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.Indian },
+                new Restaurant { Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.Mexican }
+            };
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.ToLower().Contains(name.ToLower())
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
